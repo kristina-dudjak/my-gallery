50e104d7f993f7dec0df6d097d4092c3d1f17e88
kristina <dudjakkristina@gmail.com>
2022-04-08 17:04:37 +0200

    Add component files

---
+++ src/components/Navigation.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navigation = () => {
+  return (
+    <div className="ui">
+      <div className="ui container center">
+        <h2>Navigation</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Navigation;
