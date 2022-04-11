e4f7e8230435d7f29da945ee7a1280f539e0cf43
miabekic <mia.zima.4@gmail.com>
2022-04-11 09:41:58 +0200

    Add login and register pages

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,38 @@
+import React, {useState} from 'react';
+import { Container, Form, Button} from 'react-bootstrap';
 
-function Login() {
+
+function Login(){
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
   return (
-      <div></div>
+
+    <Container className='mt-5 d-flex justify-content-center'>
+      
+    <Form className='col-md-6 p-4 border border-secondary rounded'>
+    <Form.Group className="mb-3" controlId="formEmail">
+      <Form.Control type="email" placeholder="Email" value={email} onChange={handleEmailChange} />
+    </Form.Group>
+  
+    <Form.Group className="mb-3" controlId="formPassword">
+      <Form.Control type="password" placeholder="Password" value={password} onChange={handlePasswordChange} />
+    </Form.Group>
+    <div className="d-grid gap-2">
+    <Button variant="primary" type="submit" disabled={!email || password.length<8}>
+      Login
+    </Button>
+  </div>
+    <Button variant="link">Forgote password?</Button>
+  </Form>
+  </Container>
   );
 }
-
 export default Login;
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,8 +1,55 @@
+import React, { useState } from 'react';
+import { Container, Form, Button, InputGroup } from 'react-bootstrap';
 
-function Register(){
-    return(
-        <div></div>
+
+function Registration() {
+
+    const [email, setEmail] = useState("");
+    const [name, setName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [password, setPassword] = useState("");
+
+
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value);
+    };
+
+    const handleNameChange = (event) => {
+        setName(event.target.value);
+    }
+
+    const handleLastNameChange = (event) => {
+        setLastName(event.target.value);
+    }
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+    };
+
+    return (
+        <Container className='mt-5 d-flex justify-content-center'>
+
+            <Form className='col-md-6 p-4 border border-secondary rounded'>
+                <Form.Group className="mb-2">
+                    <Form.Control type="email" placeholder="Email" onChange={handleEmailChange} />
+                </Form.Group>
+                <Form.Group className="mb-2">
+                    <Form.Control type="text" placeholder="First name" onChange={handleNameChange} />
+                </Form.Group>
+                <Form.Group className="mb-2">
+                    <Form.Control type="text" placeholder="Last name" onChange={handleLastNameChange} />
+                </Form.Group>
+                <Form.Group className="mb-2">
+                    <Form.Control type="password" placeholder="Password" onChange={handlePasswordChange} />
+                </Form.Group>
+                <div className="d-grid gap-2">
+                <Button variant="primary" type="submit" disabled={!email || !name || !lastName || password.length < 8}>
+                    Register
+                </Button>
+                </div>
+            </Form>
+        </Container>
     );
-};
+}
 
-export default Register;
+export default Registration;
