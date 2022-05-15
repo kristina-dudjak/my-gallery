0754ea37c91fc87fd94832c9b7bbfa75d3852d1c
miabekic <mia.zima.4@gmail.com>
2022-05-15 22:22:34 +0200

    Update login and registration functionality

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,22 @@
-import React, {useState} from 'react';
-import { Container, Form, Button} from 'react-bootstrap';
-import { signInWithEmailAndPassword, signOut, sendPasswordResetEmail } from "firebase/auth";
-import {auth} from '../firebase.js';
-import { useNavigate  } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Container, Form, Button, Alert } from 'react-bootstrap';
+import { signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { auth } from '../firebase.js';
+import { useNavigate } from 'react-router-dom';
+import ResetPasswordModal from './ResetPassword.js';
 
-function Login(){
+function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
-  const navigate=useNavigate();
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const navigate = useNavigate();
 
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
@@ -18,60 +26,61 @@ function Login(){
     setPassword(event.target.value);
   };
 
-  const resetPassword = () => {
-    sendPasswordResetEmail(auth, email)
-  .then(() => {
-    // Password reset email sent!
-    // ..
-    console.log("Poslano");
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    console.log(errorCode);
-    // ..
-  });
+  const emailVaildation = (event) => {
+    const pattern = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+    if(!pattern.test(email)){
+        event.preventDefault();
+        setErrorMessage("You entered invalid email. Please enter valid email.");
+    }
   }
+  const signIn = async () => {
+    try {
+      const res = await signInWithEmailAndPassword(auth, email, password);
+      if (res.user.emailVerified) {
+        navigate(-1);
+      } else {
+        await signOut(auth);
+        setErrorMessage("You didn't verified your email.")
+      }
+    } catch (error) {
+      if(error.code=="auth/wrong-password" || error.code=="auth/user-not-found"){
+        setErrorMessage("You entered wrong email and/or password. Check your email and/or password.");
+      } else{
+        setErrorMessage("Something went wrong. Please try again.")
+      }
+    }
+  }
+
   const handleSubmit = (event) => {
     setLoading(true);
     event.preventDefault();
-    signInWithEmailAndPassword(auth, email, password).then(authUser => {
-      if (authUser.user.emailVerified) { //This will return true or false
-        console.log('email is verified')
-        navigate(-1);
-
-      } else {
-        console.log('email not verified')
-        signOut(auth).then(() => {
-          console.log(auth.currentUser);
-        }
-        )
-      }
-    }).catch((error) => {
-        console.log(error);
-    });
+    signIn();
     setLoading(false);
   }
   return (
 
     <Container className='mt-5 d-flex justify-content-center'>
-      
-    <Form id='login' className='col-md-6 p-4 border border-secondary rounded' onSubmit={handleSubmit}>
-    <Form.Group className="mb-3" controlId="formEmail">
-      <Form.Control type="email" placeholder="Email" value={email} onChange={handleEmailChange} />
-    </Form.Group>
-  
-    <Form.Group className="mb-3" controlId="formPassword">
-      <Form.Control type="password" placeholder="Password" value={password} onChange={handlePasswordChange} />
-    </Form.Group>
-    <div className="d-grid gap-2">
-    <Button variant="primary" type="submit" disabled={!email || !password || loading}>
-      Login
-    </Button>
-  </div>
-    <Button variant="link" onClick={resetPassword}>Forgot password?</Button>
-  </Form>
-  </Container>
+
+      <Form id='login' noValidate className='col-md-6 p-4 border border-secondary rounded' onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="formEmail">
+          <Form.Control type="email" placeholder="Email" value={email} onChange={handleEmailChange} />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formPassword">
+          <Form.Control type="password" placeholder="Password" value={password} onChange={handlePasswordChange} />
+        </Form.Group>
+        <div className="d-grid gap-2">
+          <Button variant="primary" type="submit" disabled={!email || password<6 || loading} onClick={emailVaildation}>
+            Login
+          </Button>
+          <div className="text-danger">{errorMessage}</div>
+        </div>
+        <Button variant="link" onClick={handleShow}>Forgot password?</Button>
+        <ResetPasswordModal
+          show={show}
+          onHide={handleClose} />
+      </Form>
+    </Container>
   );
 }
 export default Login;
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Container, Form, Button } from 'react-bootstrap';
-import { createUserWithEmailAndPassword, signOut, sendEmailVerification, onAuthStateChanged } from "firebase/auth";
+import { createUserWithEmailAndPassword, signOut, sendEmailVerification} from "firebase/auth";
 import { useNavigate  } from 'react-router-dom';
 import {auth} from '../firebase.js';
 
@@ -10,6 +10,7 @@ function Registration() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [loading, setLoading] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
     const navigate=useNavigate();
 
     const handleEmailChange = (event) => {
@@ -21,51 +22,55 @@ function Registration() {
         setPassword(event.target.value);
     };
 
-    const [validated, setValidated] = useState(false);
+    const register = async () =>{
+        try{
+            const res= await createUserWithEmailAndPassword(auth, email, password);
+            await signOut(auth);
+            await sendEmailVerification(res.user);
+            navigate("/login");
+        } catch(error){
+            if(error.code=="auth/email-already-in-use") {
+            setErrorMessage("Email is already in use.");
+        } else {
+            setErrorMessage("Something went wrong. Please try again.");
+        }
+        } 
+    }
+
     const handleSubmit = (event) => {
         setLoading(true);
         event.preventDefault();
-        createUserWithEmailAndPassword(auth, email, password)
-            .then((userCredential) => {
-                // Signed in
-                signOut(auth).then(() => {
-                    sendEmailVerification(userCredential.user);
-                  navigate("/login");
-                })
-                // ...
-            })
-            .catch((error) => {
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                console.log(errorCode);
-                console.log(errorMessage);
-                // ..
-            });
+        register();
         setLoading(false);
-        setValidated(true);
     };
+
+    const emailVaildation = (event) => {
+        const pattern = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+        if(!pattern.test(email)){
+            event.preventDefault();
+            setErrorMessage("You entered invalid email. Please enter valid email.");
+        }
+
+    } 
 
 
 
     return (
         <Container className='mt-5 d-flex justify-content-center'>
 
-            <Form noValidate validated={validated} id='registration' 
-            className='col-md-6 p-4 border border-secondary rounded' onSubmit={handleSubmit}>
+            <Form noValidate id='registration' className='col-md-6 p-4 border border-secondary rounded' onSubmit={handleSubmit}>
                 <Form.Group className="mb-2">
-                    <Form.Control type="email" required placeholder="Email" onChange={handleEmailChange} 
-                    pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"/>
-                    <Form.Control.Feedback type="invalid">Please provide a valid email.</Form.Control.Feedback>
-                </Form.Group>
+                    <Form.Control type="email" required placeholder="Email" onChange={handleEmailChange} />
+                    </Form.Group>
                 <Form.Group className="mb-2">
-                    <Form.Control type="password" required placeholder="Password" onChange={handlePasswordChange} />
-                    <Form.Control.Feedback type="invalid">Password sholud have min 6 characters.</Form.Control.Feedback>
-                </Form.Group>
+                    <Form.Control type="password" required placeholder="Password (min. 6 characters)" onChange={handlePasswordChange} />
+                   </Form.Group>
                 <div className="d-grid gap-2">
-                <Button variant="primary" type="submit" disabled={!email || !password || loading}>
+                <Button variant="primary" type="submit" disabled={!email || password.length<6 || loading} onClick={emailVaildation}>
                     Register
                 </Button>
                 </div>
+                <div className="text-danger">{errorMessage}</div>
             </Form>
         </Container>
     );
