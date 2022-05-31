d6915961346a2fcbc01d5709e7fa9157351e633f
Kristina Dudjak <dudjakkristina@gmail.com>
2022-05-31 13:43:12 +0200

    Change server port

--- backend.js
+++ backend.js
@@ -1,22 +1,31 @@
-const PORT = 3000;
+const PORT = 3001;
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
 require("dotenv").config();
 const BASE_URL = "https://api.unsplash.com/";
 const key = process.env.REACT_APP_UNSPLASHED_API_KEY;
-
 const app = express();
 
 app.use(cors());
 
-app.get("/images", (req, res) => {
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.get("/images", (req, res, next) => {
   axios
     .get(BASE_URL + "photos", { params: { page: 1, client_id: key } })
     .then((response) => {
       var data = [];
       response.data.forEach((e) => {
         var imagePost = {};
+        imagePost.id = e.id;
         imagePost.smallImage = e.urls.small;
         imagePost.fullImage = e.urls.full;
         imagePost.authorName = e.user.name;
@@ -37,6 +46,7 @@ app.get("/search", (req, res) => {
       var data = [];
       response.data.results.forEach((e) => {
         var imagePost = {};
+        imagePost.id = e.id;
         imagePost.smallImage = e.urls.small;
         imagePost.fullImage = e.urls.full;
         imagePost.authorName = e.user.name;
