7422c166051cd12cd14b6f068997ba98965a99ca
miabekic <mia.zima.4@gmail.com>
2022-06-20 17:48:23 +0200

    Add route to settings page

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import MyGallery from "./pages/MyGallery";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-//import Settings from "./pages/Settings";
+import Settings from "./pages/Settings";
 import { AuthProvider } from "./contexts/AuthContext";
 import PrivateRouteGuest from "./PrivateRouteGuest";
 import PrivateRouteLogged from "./PrivateRouteLogged";
@@ -19,7 +19,7 @@ function App() {
           <Route exact path="/" element={<Home />} />
           <Route element={<PrivateRouteLogged />}>
             <Route path="/mygallery" element={<MyGallery />} />
-            {/*<Route path="/settings" element={<Settings />} />*/}
+            <Route path="/settings" element={<Settings />} />
           </Route>
           <Route path="/photos/:query" element={<Home />} />
           <Route element={<PrivateRouteGuest />}>
